4717faba9c7ac520f824d8e18d4e2ccd1eea5d29
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2024-11-18 15:05:03 +0100

    [TASK] allow translation of tt_content and tt_news as well

--- Resources/Public/JavaScript/ListLocalization.js
+++ Resources/Public/JavaScript/ListLocalization.js
@@ -1,5 +1,5 @@
 define('TYPO3/CMS/DdDeepl/ListLocalization', ['jquery'], function ($) {
-  $('.recordlist[id^="t3-table-tx_"] .col-localizationb .btn-group').each(function() {
+  $('.recordlist[id^="t3-table-t"] .col-localizationb .btn-group').each(function() {
     let that = $(this);
     let newElement = that.clone().insertAfter(that);
     let link = newElement.find('a.t3js-action-localize');
